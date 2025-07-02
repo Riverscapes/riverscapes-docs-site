a9e26423d8827042b7ba5f5d2dad875dd752c894
Jagmeet Dhillon <jagmeetdhillon@Jagmeets-MacBook-Air.local>
2025-07-02 10:14:08 -0700

    Updated paths

--- docs/.docusaurus/client-modules.js
+++
@@ -1,7 +0,0 @@
-export default [
-  require("/Users/jagmeetdhillon/Desktop/Software/developer-website-1/docs/.docusaurus/docusaurus-plugin-css-cascade-layers/default/layers.css"),
-  require("/Users/jagmeetdhillon/Desktop/Software/developer-website-1/docs/node_modules/infima/dist/css/default/default.css"),
-  require("/Users/jagmeetdhillon/Desktop/Software/developer-website-1/docs/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/Users/jagmeetdhillon/Desktop/Software/developer-website-1/docs/node_modules/@docusaurus/theme-classic/lib/nprogress"),
-  require("/Users/jagmeetdhillon/Desktop/Software/developer-website-1/docs/node_modules/@riverscapes/docusaurus-theme/dist/css/custom.css"),
-];
