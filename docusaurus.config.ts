f3db6fd93adcd1f404aa35725dc617ac71129ef2
Matt Reimer <matt@northarrowresearch.com>
2025-07-17 13:36:02 -0700

    GA and algolia

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -30,6 +30,10 @@ const config: Config = {
     [
       'classic', // Docusaurus classic preset for docs/blog
       {
+        gtag: {
+          trackingID: 'G-QH0K3VMJTD',
+          anonymizeIP: true,
+        },
         docs: {
           sidebarPath: './sidebars.ts', // Path to sidebar config
           routeBasePath: '/', // Serve docs at site root
@@ -42,6 +46,18 @@ const config: Config = {
   themeConfig: {
     image: 'img/rc_logo.png', // Social sharing image
 
+    algolia: {
+      // The application ID provided by Algolia
+      appId: '4TGS8ZPIMY',
+
+      // Public API key: it is safe to commit it
+      apiKey: 'd084a7919fe7b5940d7125f14221eaca',
+
+      indexName: 'docs.riverscapes.net',
+
+      // Optional: see doc section below
+      contextualSearch: true,
+    },
     navbar: {
       title: 'Riverscapes Docs', // Navbar title
       logo: {
